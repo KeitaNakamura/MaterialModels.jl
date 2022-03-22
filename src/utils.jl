3910d5516af5454b26fdbd0bccf8ec93620d47d2
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-03-22 10:55:58 +0900

    Add jauman2caucy matcalc function

--- src/utils.jl
+++ src/utils.jl
@@ -37,3 +37,23 @@ c = \\sqrt{\\frac{E(1-\\nu)}{\\rho (1+\\nu)(1-2\\nu)}}
     T = promote_type(typeof(E), typeof(ν), typeof(ρ))
     sqrt(E*(1-ν) / (ρ*(1+ν)*(1-2ν)))
 end
+
+"""
+    @matcalc(:jaumann2caucy; dσ_jaumann::SymmetricSecondOrderTensor{3}, σ::SymmetricSecondOrderTensor{3}, W::SecondOrderTensor{3})
+
+Convert the Jaumann stress rate to the Caucy stress rate.
+For example, this function can be used as follows:
+
+```julia
+dσᴶ = @matcalc(:stress, model; σ, dϵ = symmetric(∇v*dt)) - σ
+dσ = @matcalc(:jaumann2caucy; dσ_jaumann = dσᴶ, σ, W = skew(∇v*dt))
+```
+
+# Equation
+```math
+\\dot{\\bm{\\sigma}}^\\mathrm{J} = \\dot{\\bm{\\sigma}} - \\bm{W} \\cdot \\bm{\\sigma} + \\bm{\\sigma} \\cdot \\bm{W}
+```
+"""
+@matcalc_def function jaumann2caucy(; dσ_jaumann::SymmetricSecondOrderTensor{3}, σ::SymmetricSecondOrderTensor{3}, W::SecondOrderTensor{3})
+    dσ_jaumann + symmetric(W ⋅ σ - σ ⋅ W)
+end
