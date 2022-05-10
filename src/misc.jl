057c991676f66813f969b76077fd968cc53a0fe6
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-05-10 10:51:06 +0900

    Add functions for principal stress/strain version to LinearElastic

--- src/misc.jl
+++ src/misc.jl
@@ -57,3 +57,18 @@ dσ = @matcalc(:jaumann2caucy; dσ_jaumann = dσᴶ, σ, W = skew(∇v*dt))
 @matcalc_def function jaumann2caucy(; dσ_jaumann::SymmetricSecondOrderTensor{3}, σ::SymmetricSecondOrderTensor{3}, W::SecondOrderTensor{3})
     dσ_jaumann + symmetric(W ⋅ σ - σ ⋅ W)
 end
+
+function tospectral(σ::SymmetricSecondOrderTensor{3})
+    F = eigen(σ)
+    n₁ = F.vectors[:,1]
+    n₂ = F.vectors[:,2]
+    n₃ = F.vectors[:,3]
+    m₁ = symmetric(n₁ ⊗ n₁, :U)
+    m₂ = symmetric(n₂ ⊗ n₂, :U)
+    m₃ = symmetric(n₃ ⊗ n₃, :U)
+    F.values, m₁, m₂, m₃
+end
+
+function fromspectral(σ::Vec{3}, m₁::SymmetricSecondOrderTensor{3}, m₂::SymmetricSecondOrderTensor{3}, m₃::SymmetricSecondOrderTensor{3})
+    σ[1]*m₁ + σ[2]*m₂ + σ[3]*m₃
+end
--- test/LinearElastic.jl
+++ test/LinearElastic.jl
@@ -5,6 +5,8 @@
     stress(m, ϵ) = @matcalc(:stress, m; ϵ)
     strain(m, σ) = @matcalc(:strain, m; σ)
     stiffness(m) = @matcalc(:stiffness, m)
+    compliance(m) = @matcalc(:compliance, m)
+
     m = LinearElastic(; E = rand(), K = rand())
     @test checkparams(m, LinearElastic(; m.E, m.G))
     @test checkparams(m, LinearElastic(; m.E, m.λ))
@@ -15,9 +17,22 @@
     @test checkparams(m, LinearElastic(; m.G, m.λ))
     @test checkparams(m, LinearElastic(; m.G, m.ν))
     @test checkparams(m, LinearElastic(; m.λ, m.ν))
+
     σ = rand(SymmetricSecondOrderTensor{3})
     dϵ = rand(SymmetricSecondOrderTensor{3})
     @test @inferred(stress(m, σ, dϵ)) ≈ σ + @inferred(stress(m, dϵ))
     @test @inferred(stress(m, σ, dϵ)) ≈ σ + @inferred(stiffness(m)) ⊡ dϵ
+    @test @inferred(strain(m, σ)) ≈ @inferred(compliance(m)) ⊡ σ
     @test @inferred(stress(m, @inferred(strain(m, σ)))) ≈ σ
+
+    # principal version
+    σ3, m₁, m₂, m₃ = MaterialModels.tospectral(σ)
+    dϵ3, m₁′, m₂′, m₃′ = MaterialModels.tospectral(dϵ)
+    principal_stiffness(m) = @matcalc(:principal_stiffness, m)
+    principal_compliance(m) = @matcalc(:principal_compliance, m)
+
+    @test @inferred(stress(m, σ3, dϵ3)) ≈ σ3 + @inferred(principal_stiffness(m)) ⋅ dϵ3
+    @test @inferred(strain(m, σ3)) ≈ @inferred(principal_compliance(m)) ⋅ σ3
+    @test MaterialModels.fromspectral(@inferred(stress(m, dϵ3)), m₁′, m₂′, m₃′) ≈ stress(m, dϵ)
+    @test MaterialModels.fromspectral(@inferred(strain(m,  σ3)),  m₁,  m₂,  m₃) ≈ strain(m, σ)
 end
